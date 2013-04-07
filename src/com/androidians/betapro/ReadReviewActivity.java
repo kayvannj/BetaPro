c691c26daba3ede53af46eeb1109cd0123913037
rwwaskk <tsubomi1989@gmail.com>
2013-04-07 10:07:12 -0700

    dialog box to display pay amount‘

--- src/com/androidians/betapro/ReadReviewActivity.java
+++ src/com/androidians/betapro/ReadReviewActivity.java
@@ -11,6 +11,7 @@ import android.app.ActionBar.TabListener;
 import android.app.Activity;
 import android.app.Dialog;
 import android.app.FragmentTransaction;
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -22,11 +23,13 @@ import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-public class ReadReviewActivity extends FragmentActivity implements TabListener {
+public class ReadReviewActivity extends FragmentActivity implements TabListener,SeekBar.OnSeekBarChangeListener{
 	
 	private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
 	private static final int TAB_PUBLISH = 0;
 	private static final int TAB_MYAPPS =1;
+	TextView progView;
+	Dialog dialog;
 	
 	@Override
 	protected void onCreate(Bundle arg0) {
@@ -49,7 +52,14 @@ public class ReadReviewActivity extends FragmentActivity implements TabListener 
 		actionBar.setSelectedNavigationItem(TAB_PUBLISH);
 		
 		
-		
+		dialog = new Dialog(ReadReviewActivity.this);
+		 dialog.setContentView(R.layout.rating_dialog);
+		  dialog.setTitle("Rate and Pay");
+		  dialog.setCancelable(true);
+		  
+		  SeekBar bar = (SeekBar) dialog.findViewById(R.id.seekbar);
+		  bar.setMax(10);
+		  bar.setOnSeekBarChangeListener(this);
 		
 		Button rateBtn = (Button) findViewById(R.id.rateBtn);
 		TextView tv1 =(TextView) findViewById(R.id.detailView1);
@@ -62,13 +72,15 @@ public class ReadReviewActivity extends FragmentActivity implements TabListener 
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-			  final Dialog dialog = new Dialog(ReadReviewActivity.this);
-			  dialog.setContentView(R.layout.rating_dialog);
-			  dialog.setTitle("Rate and Pay");
-			  dialog.setCancelable(true);
+			 
+			 
 			  
-			  SeekBar bar = (SeekBar) dialog.findViewById(R.id.seekbar);
-			  bar.setMax(10);
+			  progView = (TextView) dialog.findViewById(R.id.progress);
+			
+			  
+			  
+			  
+			  
 			  
 			  Button payBtn = (Button) dialog.findViewById(R.id.payBtn);
 			  payBtn.setOnClickListener(new OnClickListener() {
@@ -108,6 +120,30 @@ public class ReadReviewActivity extends FragmentActivity implements TabListener 
 		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress,
+			boolean fromUser) {
+		// TODO Auto-generated method stub
+		progView.setTextSize(28);
+		progView.setTextColor(Color.parseColor("#FF924A"));
+		progView.setText("Suggested Pay: $"+progress);
+		
+	}
+
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 	
 	
 }
